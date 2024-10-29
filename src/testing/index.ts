e625f6ffe2291b3a1eb374a747cea1230872ce24
Traxx <31518769+crowbait@users.noreply.github.com>
2024-10-29 22:58:35 +0100

    init

---
+++ src/testing/index.ts
@@ -0,0 +1,8 @@
+import { resolvePath } from "@typespec/compiler";
+import { createTestLibrary, TypeSpecTestLibrary } from "@typespec/compiler/testing";
+import { fileURLToPath } from "url";
+
+export const TypespecUtilityTypeDecoratorsTestLibrary: TypeSpecTestLibrary = createTestLibrary({
+  name: "typespec-utility-type-decorators",
+  packageRoot: resolvePath(fileURLToPath(import.meta.url), "../../../../"),
+});
---
+++ test/test-host.ts
@@ -0,0 +1,17 @@
+import { createTestHost, createTestWrapper } from "@typespec/compiler/testing";
+import { TypespecUtilityTypeDecoratorsTestLibrary } from "../src/testing/index.js";
+
+export async function createTypespecUtilityTypeDecoratorsTestHost() {
+  return createTestHost({
+    libraries: [TypespecUtilityTypeDecoratorsTestLibrary],
+  });
+}
+
+export async function createTypespecUtilityTypeDecoratorsTestRunner() {
+  const host = await createTypespecUtilityTypeDecoratorsTestHost();
+
+  return createTestWrapper(host, {
+    autoUsings: ["TypespecUtilityTypeDecorators"]
+  });
+}
+
